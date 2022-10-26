b371a997ae6ae97fcc22ad45c4754e904f5bd31d
IvanBogachov <24term@gmail.com>
2022-10-26 12:52:13 +0300

    new pro 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,7 +5,7 @@ import { galleryItems } from './gallery-items';
 console.log(galleryItems);
 
 import SimpleLightbox from 'simplelightbox';
-import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery')
 const items = []
