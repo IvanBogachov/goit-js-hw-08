dd998bbb061e0ca625c4805a838d19eedc98b4ff
IvanBogachov <24term@gmail.com>
2022-10-27 10:52:41 +0300

    task2 no ok

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import { throttle } from "lodash";
 
+let getEL = selector => document.querySelector(selector)
+
+const form = getEL('.feedback-form');
+const email = getEL('input[name="email"]');
+const message = getEL('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(
+    'input',
+    throttle(e => {
+        const objectToSave = { email: email.value, message: message.value };
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+    }, 500
+    )
+);
+
+const load = key => {
+    try {
+        const serializedState = localStorage.getItem(key);
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+        console.error('Get state error: ', error.message);
+    }
+};
+
+const storageData = load(LOCALSTORAGE_KEY);
+if (storageData) {
+    email.value = storageData.email;
+    message.value = storageData.message;
+};
